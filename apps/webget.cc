57fd6cc2a7c39f78faf11f752df7a260d4dcae04
shenlehan <shenlehan233@gmail.com>
2026-02-01 20:02:04 +0800

    add lab1 and finish get_url

--- apps/webget.cc
+++ apps/webget.cc
@@ -11,8 +11,19 @@ using namespace std;
 namespace {
 void get_URL( const string& host, const string& path )
 {
-  debug( "Function called: get_URL( \"{}\", \"{}\" )", host, path );
-  debug( "get_URL() function not yet implemented" );
+  // debug( "Function called: get_URL( \"{}\", \"{}\" )", host, path );
+  // debug( "get_URL() function not yet implemented" );
+
+  TCPSocket sck;
+  sck.connect( Address(host, "http" ) );
+  sck.write( "GET " + path + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close + \r\n\r\n" );
+
+  string buffer;
+  while (!sck.eof()) {
+    sck.read(buffer);
+    cout << buffer;
+  }
+  sck.close();
 }
 } // namespace
 
