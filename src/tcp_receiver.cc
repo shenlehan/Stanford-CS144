a6de9cadac7cc806cba07ce8f637636ca2119e66
shenlehan <shenlehan233@gmail.com>
2026-04-20 16:23:06 +0800

    finish chpt 2

--- src/tcp_receiver.cc
+++ src/tcp_receiver.cc
@@ -12,8 +12,12 @@ void TCPReceiver::receive( TCPSenderMessage message )
     return;
   }
 
-  if (message.SYN) {
-    if (ISN_inited) {
+  if (!message.SYN && !ISN_inited) {
+    return;
+  }
+
+  if ( message.SYN ) {
+    if ( ISN_inited ) {
       return;
     }
 
@@ -21,17 +25,35 @@ void TCPReceiver::receive( TCPSenderMessage message )
     ISN_inited = true;
   }
 
-  checkpoint = reassembler_.get_first_unassembled_idx();
+  checkpoint = 1 + writer().bytes_pushed();
   uint64_t first_index = message.seqno.unwrap( zero_point, checkpoint );
-  uint64_t stream_index = first_index - 1 + (message.SYN ? 1 : 0);
+  if ( !message.SYN && first_index == 0 ) {
+    if ( message.payload.empty() ) {
+      return;
+    }
+
+    reassembler_.insert( 0, message.payload.substr( 1 ), message.FIN );
+    return;
+  }
+
+  uint64_t stream_index = first_index - 1 + ( message.SYN ? 1 : 0 );
   bool is_last = message.FIN;
   reassembler_.insert( stream_index, message.payload, is_last );
 }
 
+// send current TCPReceiver status
 TCPReceiverMessage TCPReceiver::send() const
 {
   // Your code here.
   // debug( "unimplemented send() called" );
-
-  return {};
+  TCPReceiverMessage msg;
+  if ( !ISN_inited ) {
+    msg.ackno = {};
+  } else {
+    uint64_t abs_ackno = 1 + writer().bytes_pushed() + writer().is_closed();
+    msg.ackno = Wrap32::wrap( abs_ackno, zero_point );
+  }
+  msg.RST = reader().has_error();
+  msg.window_size = std::min(writer().available_capacity(), (uint64_t)UINT16_MAX);
+  return msg;
 }
--- src/tcp_receiver.hh
+++ src/tcp_receiver.hh
@@ -8,12 +8,13 @@ class TCPReceiver
 {
 public:
   // Construct with given Reassembler
-  explicit TCPReceiver( Reassembler&& reassembler ) :
-    reassembler_( std::move( reassembler ) ),
-    zero_point( 0 ),
-    checkpoint( 0 ),
-    ISN_inited( false )
-    {}
+  explicit TCPReceiver( Reassembler&& reassembler )
+    : reassembler_( std::move( reassembler ) )
+    , zero_point( 0 )
+    , checkpoint( 0 )
+    , ISN_inited( false )
+    , window_size( reassembler_.writer().available_capacity() )
+  {}
 
   /*
    * The TCPReceiver receives TCPSenderMessages, inserting their payload into the Reassembler
@@ -35,4 +36,5 @@ private:
   Wrap32 zero_point;
   uint64_t checkpoint;
   bool ISN_inited;
+  uint16_t window_size;
 };
